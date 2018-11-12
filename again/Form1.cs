f2077fb06f2e6bb45e31be9b6e736786cecfaa83
levi12virus <levi12virus@naver.com>
2018-11-12 10:59:45 +0900

    Buttun 생성

--- again/Form1.cs
+++ again/Form1.cs
@@ -15,6 +15,26 @@ namespace again
         public Form1()
         {
             InitializeComponent();
+
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button btn = new Button();
+
+        btn.DialogResult = DialogResult.OK;//설정
+            btn.Text = "확인";
+            btn.Size = new Size(50, 50);//(x,y)
+        btn.Location = new Point(30,30);//시작점 위치이동
+
+        Controls.Add(btn);
+            btn.Click += btn_click;  //넣다 / 클릭했을때의 이벤트를
+        }
+
+        private  void btn_click(object o, EventArgs a)
+        {
+            MessageBox.Show("확인");
         }
     }
 }
