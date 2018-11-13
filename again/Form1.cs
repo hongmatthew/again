5f607f0b80b491031e05cb7b3ba7a46ba1cc192d
levi12virus <levi12virus@naver.com>
2018-11-13 13:37:27 +0900

    ctrl + v

--- again/Form1.cs
+++ again/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,44 +16,108 @@ namespace again
         public Form1()
         {
             InitializeComponent();
-
-            Load += Form1_Load;
+                Load += Form1_Load; // Form 실행 시 기본적으로 실행될 내용 호출
         }
 
         private Button btn;
+        private Label lb;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int i = 0; i < 3; i++)
+            // 가상 데이터 생성
+            ArrayList arrList = new ArrayList();
+            arrList.Add(new Item("button", 30, 30, "btn_1"));
+            arrList.Add(new Item("label", 30, 110, "lb_1"));
+            arrList.Add(new Item("button", 30, 190, "btn_2"));
+
+            for (int i = 0; i < arrList.Count; i++) // 가상 데이터를 이용한 화면 구성하기
             {
-                Button btn = new Button();
-
-                btn.DialogResult = DialogResult.OK;//설정
-                btn.Name = string.Format("btn_{0}", (i + 1));
-                btn.Text = string.Format("확인: {0}",(i+1));
-                btn.Size = new Size(100, 50);//(x,y)
-                btn.Location = new Point((100 * i) + 30, 30);//시작점 위치이동
-                btn.Cursor = Cursors.Hand;//커서모양 변경 cursor - 속성부분을 알수록 활용 넓어짐 MSDN싸이트 참고
-
-                Controls.Add(btn);
-                btn.Click += btn_click;  //넣다 / 클릭했을때의 이벤트를
+                Control ctr = Control_Create((Item)arrList[i]); // 구성될 각 화면 내용 받아오기
+                Controls.Add(ctr); // 받아온 Control 정보를 이용하여 화면 구성하기
             }
+
+            /* 1차원 배열로 화면 구성하기
+            string[] ctrList = {"button","label","button"};
+
+            for (int i = 0; i < ctrList.Length; i++)
+            {
+                if (ctrList[i] == "button")
+                {
+                    Controls.Add(btn_create(i));
+                }
+                else if (ctrList[i] == "label")
+                {
+                    Controls.Add(lb_create(i));
+                }
+            }
+            */
         }
 
-        private  void btn_click(object o, EventArgs a)//버튼에 (자기자신)대한 정보를 받아온다 boject
+        private Control Control_Create(Item item)
         {
-            //string names = "";
-            foreach(Control ct in Controls)  // foreach 배열갯수만큼 반복한다 (
+            Control ctr = new Control(); // 리턴 객체 만들기
+
+            switch (item.getType())
             {
-                //  names += ct.Name + " ";
-                if(ct.Name != "btn3") ct.BackColor = Color.Silver;
+                case "button": // button 부분 정의 하기
+                    Button btn = new Button();
+                    btn.DialogResult = DialogResult.OK;
+                    btn.Cursor = Cursors.Hand;
+                    btn.Click += btn_click;
+                    ctr = btn; // button 객체를 리턴 객체에 변경하기
+                    break;
+                case "label":
+                    Label lb = new Label();
+                    ctr = lb; // label 객체를 리턴 객체에 변경하기
+                    break;
+                default:
+                    break;
             }
-            //MessageBox.Show(namws);
+            // 리턴 객체에 공동으로 적용할 속성 정의하기
+            ctr.Name = item.getTxt();
+            ctr.Text = item.getTxt();
+            ctr.Size = new Size(100, 50);
+            ctr.Location = new Point(item.getX(), item.getY());
 
+            return ctr; // 정의 한 Control 보내기
+        }
 
-            btn = (Button) o;
+        private Button btn_create(int i) // button 객체 정의하기
+        {
+            btn = new Button();
+            btn.DialogResult = DialogResult.OK;
+            btn.Name = string.Format("btn_{0}", (i + 1));
+            btn.Text = string.Format("확인 : {0}", (i + 1));
+            btn.Size = new Size(100, 50);
+            btn.Location = new Point((100 * i) + 30, 30);
+            btn.Cursor = Cursors.Hand;
+            btn.Click += btn_click;
+            return btn;
+        }
+
+        private Label lb_create(int i) // label 객체 정의하기
+        {
+            lb = new Label();
+            lb.Name = string.Format("lb_{0}", (i + 1));
+            lb.Text = string.Format("확인 : {0}", (i + 1));
+            lb.Size = new Size(100, 50);
+            lb.Location = new Point((100 * i) + 30, 30);
+            return lb;
+        }
+
+        private void btn_click(object o, EventArgs a)
+        {
+
+            // string names = "";
+            foreach (Control ct in Controls)
+            {
+                // names += ct.Name + " ";
+                if (ct.Name != "btn_3") ct.BackColor = Color.Silver;
+            }
+            // MessageBox.Show(names);
+
+            btn = (Button)o;
             btn.BackColor = (btn.BackColor == Color.Green) ? btn.BackColor = Color.Silver : btn.BackColor = Color.Green;
-
             /*
             if (btn.BackColor == Color.Green)
             {
@@ -61,9 +126,39 @@ namespace again
             else
             {
                 btn.BackColor = Color.Green;
-            }
-            btn.BackColor = Color.Green;
+            } 
             */
         }
     }
-}
+
+    public class Item // Control 객체 생성 시 필요한 속성 정보 담을 객체 생성
+    {
+        private string type;
+        private int x;
+        private int y;
+        private string txt;
+        public Item(string type, int x, int y, string txt)
+        {
+            this.type = type;
+            this.x = x;
+            this.y = y;
+            this.txt = txt;
+        }
+        public string getType()
+        {
+            return type;
+        }
+        public int getX()
+        {
+            return x;
+        }
+        public int getY()
+        {
+            return y;
+        }
+        public string getTxt()
+        {
+            return txt;
+        }
+        }
+ }
---
+++ again/Form2.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace again
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {               //객체 사용방법!
+            InitializeComponent();
+            Load += Form2_Load1;
+        }
+
+        private void Form2_Load1(object sender, EventArgs e)
+        {
+       
+            ListView listView1 = new ListView();
+
+            ColumnHeader columnHeader1 = new ColumnHeader();
+            ColumnHeader columnHeader2 = new ColumnHeader();
+            ColumnHeader columnHeader3 = new ColumnHeader();
+            // listView1.Columns.AddRange(new ColumnHeader[] {columnHeader1,columnHeader2,columnHeader3});
+            listView1.Columns.Add(columnHeader1);
+            listView1.Columns.Add(columnHeader3);
+            listView1.Columns.Add(columnHeader2);
+
+            columnHeader1.Text = "Column 1";
+            columnHeader1.Width = 100;
+            columnHeader1.TextAlign = HorizontalAlignment.Center;
+
+            columnHeader2.Text = "Column 2";
+            columnHeader2.Width = 150;
+            columnHeader2.TextAlign = HorizontalAlignment.Left;
+
+            columnHeader3.Text = "Column 3";
+            columnHeader3.Width = 200;
+            columnHeader3.TextAlign = HorizontalAlignment.Right;
+
+            /*
+            listView1.Columns.Add("Column 1", 100, HorizontalAlignment.Center);
+            listView1.Columns.Add("Column 2", 150, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 3", 200, HorizontalAlignment.Right);
+            */
+
+            listView1.GridLines = true;
+            listView1.Location = new Point(12, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(776, 426);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+
+            Controls.Add(listView1);
+
+            /*
+            ListViewItem item1 = new ListViewItem("item1");
+            item1.SubItems.Add("1");
+            item1.SubItems.Add("2");
+            ListViewItem item2 = new ListViewItem("item2");
+            item2.SubItems.Add("4");
+            item2.SubItems.Add("5");
+            ListViewItem item3 = new ListViewItem("item3");
+            item3.SubItems.Add("7");
+            item3.SubItems.Add("8");
+
+            // listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });
+            listView1.Items.Add(item1);
+            listView1.Items.Add(item2);
+            listView1.Items.Add(item3);
+            */
+
+            // 가상 데이터 생성
+            ArrayList arry = new ArrayList();
+            arry.Add(new Items(new string[] { "item1", "1", "2" }));
+            arry.Add(new Items(new string[] { "item2", "4", "5" }));
+            arry.Add(new Items(new string[] { "item3", "7", "8" }));
+
+            // 리스트뷰 데이터 넣기
+            ListViewItem item;
+            for (int i = 0; i < arry.Count; i++)
+            {
+                Items row = (Items) arry[i];
+                item = new ListViewItem(row.Col1);
+                item.SubItems.Add(row.Col2);
+                item.SubItems.Add(row.Col3);
+                listView1.Items.Add(item);
+            }
+
+            string txts = "";
+            foreach(ColumnHeader ch in listView1.Columns)
+            {
+                txts += ch.Text + " ";
+            }
+            MessageBox.Show(txts);
+            
+        }
+    }
+
+    public class Items
+    {
+        string col1;
+        string col2;
+        string col3;
+        public Items(string[] a)
+        {
+            col1 = a[0];
+            col2 = a[1];
+            col3 = a[2];
+        }
+        public string Col1
+        {
+            get
+            {
+                return col1;
+            }
+        }
+        public string Col2
+        {
+            get
+            {
+                return col2;
+            }
+        }
+        public string Col3
+        {
+            get
+            {
+                return col3;
+            }
+        }
+        /*
+        public string getCol1()
+        {
+            return col1;
+        }
+        public string getCol2()
+        {
+            return col2;
+        }
+        public string getCol3()
+        {
+            return col3;
+        }
+        */
+    }
+}
